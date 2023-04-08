272f8b75978bd7c4da45a528a3bd889edb94000d
uswa <uswanadeem242@gmail.com>
2023-04-08 23:18:24 +0500

    simple-theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Fashion from './components/Pages/fashion/Fashion';
+import Electronic from './components/Pages/Electronic/Electronic';
+import Jewellery from './components/Pages/jewellery/Jewellery';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fashion" element={<Fashion />} />
+          <Route path="/electronic" element={<Electronic />} />
+          <Route path="/jewellery" element={<Jewellery />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
